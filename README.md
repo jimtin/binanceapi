85d7bcd960a0b2717a61949003c2641d77df5535
jimtin <neo@matrix.net>
2020-02-24 09:45:01 +1100

    Created initial binance usage page.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # binanceapi
 Library for interacting with Binance APIs (REST and wapi)
+
+# Usage
+## Getting a Binance API Key
+Information can be found here: https://www.binance.com/en/support/articles/360002502072
+
+**Note: do not share your secret key unless you know exactly what you are doing**
+**Note: Make sure you only store your public and private keys somewhere safe and secure**
+
+## Initial usage
---
+++ usebinance.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import pyfiglet # For a bit of fun :)
+import json
+
+# Create welcome banner for a bit of fun :)
+welcome_banner = pyfiglet.figlet_format("Welcome to Binancian Library")
+print(welcome_banner)
+
+PublicKey = ""
+SecretKey = ""
+
+# Get file path for keys
+# def getbinancekeys(filepath) todo: turn this into an actual function
+print("Getting filepath for Public and Secret keys. Note: DO NOT PUT KEYS DIRECTLY INTO CONSOLE")
+confirm = input("Confirm that file containing keys is in .json format (Y/N)")
+if confirm == "Y":
+    filepath = input("Put filepath here. Ensure that forward slashes (/) are used regardless of operating system")
+    # turn input into string
+    filepath = str(filepath)
+    # Use Path functionality to convert string into a filepath. Note this will allow the option to be cross platform
+    # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
+    filepath = Path(filepath)
+    # Open file
+    f = open(filepath)
+    keydatajson = f.read()
+    keydata = json.loads(keydatajson)
+    binancePublicKey = keydata['binance']['PublicKey']
+    binanceSecretKey = keydata['binance']['SecretKey']
+    PublicKey = binancePublicKey
+    SecretKey = binanceSecretKey
+    print("Binance Keys Loaded")
+else:
+    print("Create correct format then continue")
+    exit(code=0)
+
+# todo: create a way to store new keys
