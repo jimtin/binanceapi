b1fa0680787d4f5c845484a93325f199c3e235b0
jimtin <neo@matrix.net>
2020-02-27 08:39:35 +1100

    Updated main function to be a bit more user friendly

--- usebinance.py
+++ usebinance.py
@@ -2,21 +2,18 @@ from pathlib import Path
 import pyfiglet # For a bit of fun :)
 import json
 
-# Create welcome banner for a bit of fun :)
-welcome_banner = pyfiglet.figlet_format("Welcome to Binancian Library")
-print(welcome_banner)
-
-PublicKey = ""
-SecretKey = ""
 
 # Get file path for keys
-# def getbinancekeys(filepath) todo: turn this into an actual function
-print("Getting filepath for Public and Secret keys. Note: DO NOT PUT KEYS DIRECTLY INTO CONSOLE")
-confirm = input("Confirm that file containing keys is in .json format (Y/N)")
-if confirm == "Y":
-    filepath = input("Put filepath here. Ensure that forward slashes (/) are used regardless of operating system")
-    # turn input into string
-    filepath = str(filepath)
+def main(filepath=""):
+    # Create welcome banner for a bit of fun :)
+    welcome_banner = pyfiglet.figlet_format("Welcome to Binancian Library")
+    print(welcome_banner)
+    #print("Getting filepath for Public and Secret keys. Note: DO NOT PUT KEYS DIRECTLY INTO CONSOLE")
+    if filepath == "":
+        filepath = input("Put filepath here. Ensure that forward slashes (/) are used regardless of operating system")
+    else:
+        filepath = filepath
+
     # Use Path functionality to convert string into a filepath. Note this will allow the option to be cross platform
     # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
     filepath = Path(filepath)
@@ -28,11 +25,10 @@ if confirm == "Y":
     binanceSecretKey = keydata['binance']['SecretKey']
     PublicKey = binancePublicKey
     SecretKey = binanceSecretKey
+    Keys = [PublicKey, SecretKey]
     print("Binance Keys Loaded")
-else:
-    print("Create correct format then continue")
-    exit(code=0)
+    return keydata['binance']
 
-# todo: create a way to store new keys
+    # todo: create a live query mode using the python 'blessed' library
 
-# todo: figure out a way to encrypt the secret key in memory
+# todo: figure out a way to encrypt the secret key in memory. Will probably need to change programming languages
