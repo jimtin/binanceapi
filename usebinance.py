416dfdfad9c2eb870018eebeecbe3f470aa64934
jimtin <neo@matrix.net>
2020-03-01 09:10:58 +1100

    Adding ability to query splunk database

--- usebinance.py
+++ usebinance.py
@@ -8,37 +8,47 @@ import datetime
 import coinbaselibrary
 
 
-# Global Variables
-SplunkSettingsFilepath = ""
-BinanceKeysFilePath = ""
-
-
 # Get file path for keys
-def main(BinanceFilepath="", SplunkSettings=""):
+def main(BinanceFilepath="", SplunkSettings="", TimeWindow=60):
     # Create welcome banner for a bit of fun :)
     welcome_banner = pyfiglet.figlet_format("Welcome to Binancian Library")
     print(welcome_banner)
 
-    # Get the filepaths for the settings
-    # Use Path functionality to convert string into a filepath. Note this will allow the option to be cross platform
+    # Setup the filepaths for getting settings
+    # First: Get Binance keys
+    if BinanceFilepath == "":
+        print("No value supplied for Binance keys, please input values below")
+        BinanceFilepath = input("Put Binance Settings filepath here. Ensure that forward slashes (/) are used regardless of operating system")
+    # Else statement just to ensure everything is captured
+    else:
+        BinanceFilepath = BinanceFilepath
+    # Now turn into a Path variable using Path library. This allows it to be platform agnostic
     # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
-    BinanceFilepath = input("Put Binance Settings filepath here. Ensure that forward slashes (/) are used regardless of operating system")
     BinanceKeysFilePath = Path(BinanceFilepath)
-    SplunkSettings = input("Put SplunkSettings Filepath here. Ensure that forward slashes (/) are used regardless of operating system")
+
+    # Second: Get Splunk settings
+    if SplunkSettings == "":
+        print("No value supplied for Splunk settings, please input values below")
+        SplunkSettings = input("Put SplunkSettings Filepath here. Ensure that forward slashes (/) are used regardless of operating system")
+    # Else statement just to ensure everything is captured
+    else:
+        SplunkSettings = SplunkSettings
+    # Now turn into a Path variable using Path library. This allows it to be platform agnostic
     SplunkSettingsFilepath = Path(SplunkSettings)
 
+    # Load the settings into the file
     binancekeys = getbinancekeys(BinanceKeysFilePath)
     print("Binance Keys Loaded")
     splunksettings = splunk_as_a_database.getsplunksettings(SplunkSettingsFilepath)
     print("Splunk settings loaded")
-    print("Starting datagathering exercise")
-    datagathering(binancekeys, splunksettings, 60)
+
+    # Now start running the program
+    print("Getting crypto prices")
+    cryptopricemovementgathering(binancekeys, splunksettings)
 
 
 # Function to start collecting data from binance for practice. Set to run each hour.
-def datagathering(binanceKeys, splunkSettings, timewindow):
-    #while 1:
-    # First get data from binance
+def cryptopricemovementgathering(binanceKeys, splunkSettings):
     print("Getting binance data " + str(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
     pricechangedata = binancelibrary.getpricechanges()
     # Now send binance data to splunk
@@ -47,10 +57,8 @@ def datagathering(binanceKeys, splunkSettings, timewindow):
     # Now get coinbase data
     print("Getting coinbase data " + str(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
     spotpricedata = coinbaselibrary.combinespotprices()
-    print(spotpricedata)
     print("Sending coinbase data to splunk")
     sendtosplunk(spotpricedata, "coinbase", splunkSettings)
-    print("Sleeping for an hour now")
 
 
 # Function to get binance keys from the BinanceFilePath
